ac03300388db6bcbf5cd2edc64cbc93a744d458d
James <jamesseventyseven@gmail.com>
2020-06-30 05:21:52 +0100

    Modified stories.py

--- stories.py
+++ stories.py
@@ -1,18 +1,5 @@
-from flask import Flask, request, render_template
-from flask_debugtoolbar import DebugToolbarExtension
-app = Flask(__name__)
-app.config['SECRET_KEY'] = "oh-so-secret"
 
-debug = DebugToolbarExtension(app)
 """Madlibs Stories."""
-
-
-@app.route('/')
-def show_home():
-    story = Story(["place", "noun", "verb", "adjective", "plural_noun"],
-                  """Once upon a time in a long-ago {place}, there lived a large {adjective} {noun}. It loved to {verb} {plural_noun}.""")
-
-    return render_template('home.html')
 
 
 class Story:
